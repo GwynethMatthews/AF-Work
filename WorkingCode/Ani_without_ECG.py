20502520ad0fd2148b382695ae530ae25187ccb9
Jack Way <jackway96@gmail.com>
2019-02-12 15:19:13 +0000

    Added stuff

--- WorkingCode/Ani_without_ECG.py
+++ WorkingCode/Ani_without_ECG.py
@@ -33,16 +33,15 @@ convolve = True
 grey_background = False
 resting_cells = False
 
-seed1 = 1485598097
-seed2 = 1709896889
+seed1 = 1686038309
+seed2 = 2003253072
 nu = 0.54
 
 
-A = AC.SourceSinkModel(hexagonal=True, threshold=1, p_nonfire=0.15, pace_rate= 120,
+A = AC.SourceSinkModel(hexagonal=True, threshold=1, p_nonfire=0.54, pace_rate= 120,
                        Lx=100, Ly=100, tot_time= 10000, nu_para=nu, nu_trans=nu, rp = 110,
                        seed_connections=seed1, seed_prop=seed2, boundary = True, 
-                       pacemaker_line = True, radius = 3, charge_conservation = False,
-                       t_under = 1, t_under_on = False)
+                       charge_conservation = False, t_under = 1, t_under_on = True)
 
 
 ###############################################################################
